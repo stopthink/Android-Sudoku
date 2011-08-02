78ebfa50a72c7cd0aed115b08ff8f4340cf2a442
David Alexander <sophistihip@gmail.com>
2011-08-01 21:55:33 -0400

    adding some resources for the game board and extending the game class

---
+++ src/org/example/sudoku/PuzzleView.java
@@ -0,0 +1,54 @@
+package org.example.sudoku;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Paint.FontMetrics;
+import android.graphics.Paint.Style;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+
+
+public class PuzzleView extends View {
+	private static final String TAG = "Sudoku";
+	private final Game game;
+	
+	private float width; // width of one tile
+	private float height; // height of one tile
+	private int selX; // x index of selection
+	private int selY; // y index of selection
+	private final Rect selRect = new Rect();
+	
+	public PuzzleView(Context context) {
+		super(context);
+		this.game = (Game) context;
+		setFocusable(true);
+		setFocusableInTouchMode(true);
+	}
+
+	
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		width = w / 9f;
+		height = h / 9f;
+		getRect(selX, selY, selRect);
+		Log.d(TAG, "onSizeChanged: width " + width + ", height " + height);
+		super.onSizeChanged(w, h, oldw, oldh);
+	}
+	
+	private void getRect(int x, int y, Rect rect) {
+		rect.set((int) (x * width), (int) (y * height), (int) (x * width + width), (int) (y * height + height));
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// Draw the background
+		Paint background = new Paint();
+		background.setColor(getResources().getColor(R.color.puzzle_background));
+		canvas.drawRect(0, 0, getWidth(), getHeight(), background);
+	}
+}
